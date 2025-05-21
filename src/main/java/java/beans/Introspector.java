19e34efc3d6149ba6228072dcf31dd8cc73b94e1
Deenbandhu Netam <deenbandhunetam488@gmail.com>
2025-05-21 12:32:06 +0530

    add original method

--- src/main/java/java/beans/Introspector.java
+++ src/main/java/java/beans/Introspector.java
@@ -2,7 +2,17 @@ package java.beans;
 
 public class Introspector {
 
-    public static String decapitalize(String string) {
-        return string.toLowerCase();
+    public static String decapitalize(String name) {
+    if (name == null || name.length() == 0) {
+      return name;
     }
+    if (name.length() > 1
+        && Character.isUpperCase(name.charAt(1))
+        && Character.isUpperCase(name.charAt(0))) {
+      return name;
+    }
+    char chars[] = name.toCharArray();
+    chars[0] = Character.toLowerCase(chars[0]);
+    return new String(chars);
+  }
 }
